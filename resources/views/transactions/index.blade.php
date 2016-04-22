13a4459688955ede7de62803c4ff75153a3f10c1
Webneoo <info@webneoo.com>
2016-04-22 11:53:37 +0300

    nour: change number format in invoice and transaction

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -58,10 +58,10 @@
                                 </td>
                                 <td>{{ $t->description }}</td>
                                 @if($t->type==0) 
-                                    <td style="color:#d9534f">$ {{ number_format($i->amount, 2, '.', ' ') }}</td>
+                                    <td style="color:#d9534f">$ {{ number_format($t->amount, 2, '.', ' ') }}</td>
                                     <td style="color:#d9534f"><i class="fa fa-arrow-up"></i> &nbsp&nbsp OUT </td>
                                 @elseif($t->type==1)
-                                    <td style="color:#5cb85c">$ {{ number_format($i->amount, 2, '.', ' ') }}</td>
+                                    <td style="color:#5cb85c">$ {{ number_format($t->amount, 2, '.', ' ') }}</td>
                                     <td style="color:#5cb85c"><i class="fa fa-arrow-down"></i> &nbsp&nbsp IN </td>
                                 @endif
                                 <td>
