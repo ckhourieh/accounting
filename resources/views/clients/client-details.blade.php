845dd2ebad06071b64fb38eccc5942657f89a702
Michel <michel.achkar@webneoo.com>
2016-12-15 18:07:49 +0200

    Send emails to NOT black invoices only

--- resources/views/clients/client-details.blade.php
+++ resources/views/clients/client-details.blade.php
@@ -128,10 +128,6 @@
                         </table>
                     </div>
                 </div>
-                <div class="panel-heading">Timeline</div>
-                <div class="panel-body">
-                    <div id="morris-bar-chart"></div>
-                </div>
           
         </div>
     </div>
@@ -152,84 +148,6 @@
 <script type="text/javascript" src="/js/html2canvas.js"></script>
 <script type="text/javascript" src="/js/download.js"></script>
 
-<!-- Morris Chart Js -->
-<script src="/js/morris/raphael.min.js"></script>
-<script src="/js/morris/morris.min.js"></script>
 
-<script type="text/javascript">
-    $(document).ready(function () {
-        // I changed the selector of table to select all the tables except the first one
-
-        $(".table:not(:first)").DataTable({
-            "order": [[ 0, "desc" ]]
-        });
-        $(".btnPrint").printPage();
-
-        /* MORRIS BAR CHART
-        -----------------------------------------*/
-        Morris.Bar({
-            element: 'morris-bar-chart',
-            data: [{
-                y: 'JAN',
-                a: 100,
-                b: 90
-            }, {
-                y: 'FEB',
-                a: 75,
-                b: 65
-            }, {
-                y: 'MAR',
-                a: 50,
-                b: 40
-            }, {
-                y: 'APR',
-                a: 75,
-                b: 65
-            }, {
-                y: 'MAY',
-                a: 50,
-                b: 40
-            }, {
-                y: 'JUN',
-                a: 75,
-                b: 65
-            }, {
-                y: 'JUL',
-                a: 100,
-                b: 90
-            }, {
-                y: 'AUG',
-                a: 50,
-                b: 40
-            }, {
-                y: 'SEP',
-                a: 75,
-                b: 65
-            }, {
-                y: 'OCT',
-                a: 50,
-                b: 40
-            }, {
-                y: 'NOV',
-                a: 75,
-                b: 65
-            }, {
-                y: 'DEC',
-                a: 100,
-                b: 90
-            }],
-            xkey: 'y',
-            ykeys: ['a', 'b'],
-            labels: ['Income', 'Outcome'],
-             barColors: [
-        '#5cb85c','#d9534f',
-        '#A8E9DC' 
-        ],
-            hideHover: 'auto',
-            resize: true
-        });
-
-    });
-</script>
 
 @endsection
